14db92cf89a666585f3e3451ce86109a20fcdad8
AhmeD-GabeR0 <ahmedig22@gmail.com>
2017-07-11 03:09:15 +0200

    styling

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,11 @@
-import { Recipe } from "app/recipes/recipe.model";
+import { Recipe } from 'app/recipes/recipe.model';
 
 export class RecipeService {
    private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
     new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
   ];
-  getRecipe(){
+  getRecipe() {
    return this.recipes.slice(); // to create a new copy of the array of recipes so that when we change
    // it not changed the recipe array in recipe service
   }
